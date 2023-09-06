86283c39d1fa33ffe4146755b677a9fe966bbd93
François Billet <francoisbillet@live.fr>
2023-09-06 22:21:08 +0200

    v3: useMutation - no refresh

--- src/hooks/useCreatePost.ts
+++ src/hooks/useCreatePost.ts
@@ -1,22 +1,14 @@
-import { useMutation, useQueryClient } from "react-query";
+import { useMutation } from "react-query";
 import { PostRequest } from "../types/posts";
 
 export function useCreatePost() {
-  const queryClient = useQueryClient();
-
-  return useMutation(
-    (newPost: PostRequest) =>
-      fetch("http://localhost:3000/api/posts/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newPost),
-      }).then((res) => res.json()),
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ["posts"] });
+  return useMutation((newPost: PostRequest) =>
+    fetch("http://localhost:3000/api/posts/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    }
+      body: JSON.stringify(newPost),
+    }).then((res) => res.json())
   );
 }
